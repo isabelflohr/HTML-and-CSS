93bdfa6b4d34a0af9e8de5293b1fadfcb5e8ff2b
IsabelFlohr <isabelflohr@gmail.com>
2020-04-01 17:13:03 -0700

    edits

---
+++ Basic_HTML_and_JavaScript_1/JSvalidation_form.js
@@ -0,0 +1,32 @@
+function validateForm() {
+    var x =document.forms["phoneNum"]["phonenum"].value;
+    if (x== "") {
+        alert("Enter cell number to Submit");
+        return false;
+    }
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+// SLIDESHOW
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000);
+}
